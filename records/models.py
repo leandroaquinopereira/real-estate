3f4b2960fbf46a695b2e8bde636d68a82608152a
leandroaquinopereira <leandroaquinopereira@outlook.com>
2022-01-07 16:27:33 -0300

    Edit models

--- records/models.py
+++ records/models.py
@@ -5,8 +5,9 @@ from django.db import models
 class Property(models.Model):
 
     type = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    value = models.CharField(max_length=15)
+    address = models.CharField(max_length=150, unique=True)
+    value = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
 
     def __str__(self):
         return self.type
@@ -18,8 +19,9 @@ class Property(models.Model):
 class Client(models.Model):
 
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=50)
-    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=80)
+    phone = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
         return self.name
@@ -28,10 +30,12 @@ class Rent(models.Model):
 
     start = models.DateField()
     end = models.DateField()
+    finished = models.BooleanField(default=False, help_text='Check')
 
-    client = models.ForeignKey(Client, on_delete=models.PROTECT)
-    property = models.OneToOneField(Property, on_delete=models.PROTECT)
+    client = models.ForeignKey(Client, on_delete=models.PROTECT, null=True, blank=False)
+    property = models.OneToOneField(Property, on_delete=models.PROTECT, null=True, blank=False)
 
     def __str__(self):
-        return self.property.type
+        return  'Contrato ' + str(self._get_pk_val()) + ': ' + self.property.type + ' - ' + self.client.name + ' | ' + self.finished
+
 
