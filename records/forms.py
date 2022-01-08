5fba1e291fe34a98c4612fb67f86f437d57cf4f1
leandroaquinopereira <leandroaquinopereira@outlook.com>
2022-01-08 11:37:36 -0300

    Upgrade forms

--- records/forms.py
+++ records/forms.py
@@ -6,19 +6,16 @@ from records.models import Client, Property, Rent
 class PropertyForm(forms.ModelForm):
     class Meta:
         model = Property
-        # fields = ['nome', 'capital']
         fields = '__all__'
 
 
 class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
-        # fields = ['nome', 'capital']
         fields = '__all__'
 
 
 class RentForm(forms.ModelForm):
     class Meta:
         model = Rent
-        # fields = ['nome', 'capital']
         fields = '__all__'
