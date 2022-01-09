880b26b8e727bed61de43ff7d64e3945b7de800f
leandroaquinopereira <leandroaquinopereira@outlook.com>
2022-01-09 12:55:44 -0300

    Add URL Properties

--- records/urls.py
+++ records/urls.py
@@ -1,12 +1,19 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import include, path
 
-from records.views import ClientList, ClientDetail, ClientDelete, ClientCreate, ClientUpdate
+from records.views import ClientList, ClientDetail, ClientDelete, ClientCreate, ClientUpdate, PropertyList, \
+    PropertyDetail, PropertyDelete, PropertyCreate, PropertyUpdate
 
 urlpatterns = [
-    path('', ClientList.as_view(), name='client-list'),
-    path('detail/<int:pk>/', ClientDetail.as_view(), name='client-detail'),
-    path('delete/<int:pk>/', login_required(ClientDelete.as_view()), name='client-delete'),
-    path('create/', login_required(ClientCreate.as_view()), name='client-create'),
-    path('update/<int:pk>/', login_required(ClientUpdate.as_view()), name='client-update')
+    path('client/', ClientList.as_view(), name='client-list'),
+    path('client/detail/<int:pk>/', ClientDetail.as_view(), name='client-detail'),
+    path('client/delete/<int:pk>/', login_required(ClientDelete.as_view()), name='client-delete'),
+    path('client/create/', login_required(ClientCreate.as_view()), name='client-create'),
+    path('client/update/<int:pk>/', login_required(ClientUpdate.as_view()), name='client-update'),
+
+    path('property/', PropertyList.as_view(), name='property-list'),
+    path('property/detail/<int:pk>/', PropertyDetail.as_view(), name='property-detail'),
+    path('property/delete/<int:pk>/', login_required(PropertyDelete.as_view()), name='property-delete'),
+    path('property/create/', login_required(PropertyCreate.as_view()), name='property-create'),
+    path('property/update/<int:pk>/', login_required(PropertyUpdate.as_view()), name='property-update')
 ]
